195325bda31d53ab8c30a319e160b0aad3218ab9
NGibbs28 <ngibbs052006@gmail.com>
2023-05-22 20:39:49 +0100

    V 0.0.3
    Prices and Trades Finalised

--- src/main/java/net/nathan/testmod/villager/ModVillagers.java
+++ src/main/java/net/nathan/testmod/villager/ModVillagers.java
@@ -45,9 +45,78 @@ public class ModVillagers {
                 factories -> {
                     factories.add(((entity, random) -> new TradeOffer(
                             new ItemStack(Items.EMERALD, 10),
-                            new ItemStack(Items.COAL, 5),
+                            new ItemStack(Items.COBBLESTONE, 64),
+                            10,2,0.02f
+                    )));
+                    factories.add(((entity, random) -> new TradeOffer(
+                            new ItemStack(Items.EMERALD, 15),
+                            new ItemStack(Items.STONE, 64),
+                            10,2,0.02f
+                    )));
+                    factories.add(((entity, random) -> new TradeOffer(
+                            new ItemStack(Items.EMERALD, 15),
+                            new ItemStack(Items.DEEPSLATE, 64),
                             10,2,0.02f
                     )));
                 });
+
+        TradeOfferHelper.registerVillagerOffers(JUMP_MASTER, 2,
+                factories -> {
+                    factories.add(((entity, random) -> new TradeOffer(
+                            new ItemStack(Items.EMERALD, 10),
+                            new ItemStack(Items.COAL_ORE, 7),
+                            10,3,0.02f
+                    )));
+                    factories.add(((entity, random) -> new TradeOffer(
+                            new ItemStack(Items.EMERALD, 10),
+                            new ItemStack(Items.COPPER_ORE, 6),
+                            10,3,0.02f
+                    )));
+                });
+
+        TradeOfferHelper.registerVillagerOffers(JUMP_MASTER, 3,
+                factories -> {
+                    factories.add(((entity, random) -> new TradeOffer(
+                            new ItemStack(Items.EMERALD, 20),
+                            new ItemStack(Items.IRON_ORE, 15),
+                            10,5,0.02f
+                    )));
+                    factories.add(((entity, random) -> new TradeOffer(
+                            new ItemStack(Items.EMERALD, 20),
+                            new ItemStack(Items.REDSTONE_ORE, 5),
+                            10,5,0.02f
+                    )));
+                });
+
+        TradeOfferHelper.registerVillagerOffers(JUMP_MASTER, 4,
+                factories -> {
+                    factories.add(((entity, random) -> new TradeOffer(
+                            new ItemStack(Items.EMERALD, 25),
+                            new ItemStack(Items.LAPIS_ORE, 2),
+                            10,5,0.02f
+                    )));
+                    factories.add(((entity, random) -> new TradeOffer(
+                            new ItemStack(Items.EMERALD, 25),
+                            new ItemStack(Items.GOLD_ORE, 20),
+                            10,5,0.02f
+                    )));
+                });
+
+        TradeOfferHelper.registerVillagerOffers(JUMP_MASTER, 5,
+                factories -> {
+                    factories.add(((entity, random) -> new TradeOffer(
+                            new ItemStack(Items.EMERALD, 64),
+                            new ItemStack(Items.DIAMOND_ORE, 1),
+                            10,2,0.05f
+                    )));
+                    factories.add(((entity, random) -> new TradeOffer(
+                            new ItemStack(Items.EMERALD, 50),
+                            new ItemStack(Items.DIAMOND, 1),
+                            10,2,0.05f
+                    )));
+                });
+
+
     }
 }
+
